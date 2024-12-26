6bd91a893be535c31003127917ee16decf93135c
Bruno Hernandez <hzzbruno@gmail.com>
2024-12-26 00:46:14 -0300

    new look

--- js/language.js
+++ js/language.js
@@ -3,94 +3,98 @@ document.addEventListener("DOMContentLoaded", () => {
     const languageToggle = document.querySelector("#language-toggle");
     const texts = {
         en: {
-            about: "About Me",
-            aboutp: "I am a Software Engineering student currently in the fourth year of my degree. I have a strong talent for self-research and self-education, which helps me stay updated and continuously improve my skills. Additionally, I work at my family's tech and general goods store in my local city, where I am actively involved as a customer service advisor,assisting clients and ensuring a positive experience.",
-            knowledge: "My Knowledge",
-            projects: "Recent Projects",
-            contact: "Contact",
-            heroSubtitle: "Software Developer",
-            button: "View Projects",
-            utu:"Developed in my last year of high school, it is a Unity game made up of a collection of minigames. The project involved creating interactive mechanics, engaging visual design, and a well-organized structure to ensure a fluid and dynamic experience for players.",
-            vuyd:"Airline system with a central server in Java (Swing) and a web server (JSP, servlets), synchronized through web services. Allows customers to book flights and airlines to manage routes and flights, with changes reflected simultaneously on both servers. It uses OOP, design patterns and custom base types.",
-            vuye:"Link to representative video of the page",
-            centralT:"VolandoUY Central Server",
+            linkabout: "About me",
+            linkprojects: "Projects",
+            linkcontact: "Contact",
+
+            hello: "Hi, I'm",
+            proffesion: "Software Developer",
+            btnResume: "Download CV",
+
+            aboutMe: "About Me",
+            aboutMeDescription: "Hello, my name is Bruno Hernández. I am a Software Engineer Student from Uruguay.<br>I have a strong talent for self-research and self-education, which helps me stay updated and continuously improve my skills.Additionally, I work at my family's tech and general goods store in my local city,<br> where I am actively involved as a customer service advisor,assisting clients and ensuring a positive experience.",
+            knowledge: "Knowledge",
+
+            recentProjects: "Recent Projects",
+
+            VolandoUYDescripcion: "Airline system with a central server in Java (Swing) and a web server (JSP, servlets), synchronized through web services. Allows customers to book flights and airlines to manage routes and flights, with changes reflected simultaneously on both servers. It uses OOP, design patterns and custom base types.",
+            dessVUY: "Developed with HTML, CSS , JavaScript, Servlets, Java, Maven",
+
+            UTU:"Developed in my last year of high school, it is a Unity game made up of a collection of minigames. The project involved creating interactive mechanics, engaging visual design, and a well-organized structure to ensure a fluid and dynamic experience for players.",
+            dessUTU:"Developed with C# in Unity",
+
             centralD:"Developed in Swing, this component represents the central server of the project, responsible for managing the main logic and starting the web services for communication with other modules.",
-            vuyn:"The image leads to a static page with the intention of showing the proposed base design.",
+            dessVUYC:"Developed with Java and Swing in eclipse",
 
-            nombreC:"Name",
-            mensajeC:"Message",
-            enviarC:"Send",
+            contactTitle: "Contact Me",
+            nombreContacto: "Name:",
+            mensajeContacto:"Message:",
+            botonEnviar:"Send",
 
-            linkabout:"About Me",
-            linkknowledge:"Knowledge",
-            linkprojects:"Recent Projects",
-            linkcontact:"Contact",
-
-            heroTitle2:"Hi, I'm ",
-
-            DUTUT:"Developed with C# in Unity",
-            DVUY:"Developed with HTML, CSS , JavaScript, Servlets, Java, Maven",
-            DVUYC:"Developed with Java and Swing in eclipse",
         },
         es: {
-            about: "Sobre mí",
-            aboutp: "Soy estudiante de Ingeniería de Software actualmente en el cuarto año de la carrera. Tengo un gran talento para la autoinvestigación y la autoeducación, lo que me ayuda a mantenerme actualizado y mejorar continuamente mis habilidades. Además, trabajo en la tienda de artículos generales y de tecnología de mi familia en mi ciudad local, donde participo activamente como asesor de servicio al cliente, asistiendo a los clientes y garantizando una experiencia positiva.",
+            linkabout: "Sobre Mi",
+            linkprojects: "Projectos",
+            linkcontact: "Contacto",
+
+            hello: "Hola, soy",
+            proffesion: "Desarrollador de Software",
+            btnResume: "Descargar CV",
+
+            aboutMe: "Sobre Mi",
+            aboutMeDescription: "Hola, mi nombre es Bruno Hernández. Soy estudiante de Ingeniería de Software de Uruguay.<br>Tengo un gran talento para la autoinvestigación y la autoeducación, lo que me ayuda a mantenerme actualizado y mejorar continuamente mis habilidades.<br>Además, trabajo en la tienda de tecnología y artículos generales de mi familia en mi ciudad local,donde participo activamente como asesor de servicio al cliente, ayudándolos y garantizando una experiencia positiva.",
             knowledge: "Mis Conocimientos",
-            projects: "Proyectos Recientes",
-            contact: "Contacto",
-            heroSubtitle: "Desarrollador de Software",
-            button: "Ver Proyectos",
-            utu:"Desarrollado en mi último año de secundaria, es un juego en Unity compuesto por una colección de minijuegos. El proyecto involucró la creación de mecánicas interactivas, diseño visual atractivo y una estructura bien organizada para garantizar una experiencia fluida y dinámica para los jugadores.",
-            vuyd:"Sistema de aerolíneas con un servidor central en Java (Swing) y un servidor web (JSP, servlets), sincronizados mediante web services. Permite a los clientes reservar vuelos y a las aerolíneas gestionar rutas y vuelos, con cambios reflejados simultáneamente en ambos servidores. Utiliza POO, patrones de diseño y tipos básicos personalizados.",
-            vuye:"Enlace a video representativo de la página",
-            centralD:"Desarrollado en Swing, este componente representa el servidor central del proyecto, encargado de gestionar la lógica principal y de iniciar los web services para la comunicación con otros módulos.",
-            centralT:"Servidor Central de VolandoUY",
-            vuyn:"La imagen lleva a una página estática con la intención de mostrar el diseño base planteado.",
-            
-            nombreC:"Nombre",
-            mensajeC:"Mensaje",
-            enviarC:"Enviar",
 
-            linkabout:"Sobre Mi",
-            linkknowledge:"Mis Conocimientos",
-            linkprojects:"Proyectos Recientes",
-            linkcontact:"Contacto",
+            recentProjects: "Projectos Recientes",
 
-            heroTitle2:"Hola, soy ",
+            VolandoUYDescripcion: "Sistema de aerolínea con un servidor central en Java (Swing) y un servidor web (JSP, servlets), sincronizados mediante servicios web. Permite a los clientes reservar vuelos y a las aerolíneas gestionar rutas y vuelos, con los cambios reflejados simultáneamente en ambos servidores. Utiliza programación orientada a objetos, patrones de diseño y tipos de base personalizados.",
+            dessVUY: "Desarrollado con HTML, CSS , JavaScript, Servlets, Java, Maven",
 
-            DUTUT:"Desarrollado con C# en Unity",
-            DVUY:"Desarrollado con HTML, CSS, JavaScript, Servlets, Java, Maven",
-            DVUYC:"Desarrollado con Java y Swing en eclipse.",
+            UTU:"Desarrollado en mi último año de secundaria, es un juego de Unity compuesto por una colección de minijuegos. El proyecto implicó la creación de mecánicas interactivas, un diseño visual atractivo y una estructura bien organizada para garantizar una experiencia fluida y dinámica para los jugadores.",
+            dessUTU:"Desarrollado con C# en Unity",
+
+            centralD:"Desarrollado en Swing, este componente representa el servidor central del proyecto, responsable de gestionar la lógica principal e iniciar los servicios web para la comunicación con otros módulos.",
+            dessVUYC:"Desarrollado con Java y Swing en eclipse",
+
+            contactTitle: "Contactame",
+            nombreContacto: "Nombre:",
+            mensajeContacto:"Mensaje:",
+            botonEnviar:"Enviar",
+           
         },
     };
 
     languageToggle?.addEventListener("change", (event) => {
         const lang = event.target.value;
-        document.querySelector("#UTU").textContent = texts[lang].utu;
-        document.querySelector("#VolandoUYDescripcion").textContent = texts[lang].vuyd;
-        document.querySelector("#centralD").textContent = texts[lang].centralD;
-
-        document.querySelector("#nombreContacto").textContent = texts[lang].nombreC;
-        document.querySelector("#mensajeContacto").textContent = texts[lang].mensajeC;
-        document.querySelector("#botonEnviar").textContent = texts[lang].enviarC;
-
         document.querySelector("#linkabout").textContent = texts[lang].linkabout;
-        document.querySelector(".titleK h2").textContent = texts[lang].linkknowledge;
-        document.querySelector(".titleP h2").textContent = texts[lang].linkprojects;
+        document.querySelector("#linkprojects").textContent = texts[lang].linkprojects;
         document.querySelector("#linkcontact").textContent = texts[lang].linkcontact;
 
-        document.querySelector("#about h2").textContent = texts[lang].about;
-        document.querySelector("#about p").textContent = texts[lang].aboutp;
+        document.querySelector("#hello").textContent = texts[lang].hello;
+        document.querySelector("#proffesion").textContent = texts[lang].proffesion;
+        document.querySelector("#btnResume").textContent = texts[lang].btnResume;
 
-        document.querySelector("#contact h2").textContent = texts[lang].contact;
-        document.querySelector("#botoncontacto").textContent = texts[lang].contact;
+        document.querySelector("#aboutMe").textContent = texts[lang].aboutMe;
+        document.querySelector("#aboutMeDescription").innerHTML = texts[lang].aboutMeDescription;
+        document.querySelector("#knowledge").textContent = texts[lang].knowledge;
 
-        document.querySelector("#hero h2").textContent = texts[lang].heroTitle2;
-        document.querySelector("#hero p").textContent = texts[lang].heroSubtitle;
+        document.querySelector("#recentProjects").textContent = texts[lang].recentProjects;
 
-        document.querySelector("#dessUTU").textContent = texts[lang].DUTUT;
-        document.querySelector("#dessVUY").textContent = texts[lang].DVUY;
-        document.querySelector("#dessVUYC").textContent = texts[lang].DVUYC;
+        document.querySelector("#VolandoUYDescripcion").textContent = texts[lang].VolandoUYDescripcion;
+        document.querySelector("#dessVUY").textContent = texts[lang].dessVUY;
+
+        document.querySelector("#UTU").textContent = texts[lang].UTU;
+        document.querySelector("#dessUTU").textContent = texts[lang].dessUTU;
+
+        document.querySelector("#centralD").textContent = texts[lang].centralD;
+        document.querySelector("#dessVUYC").textContent = texts[lang].dessVUYC;
+
+        document.querySelector("#contactTitle").textContent = texts[lang].contactTitle;
+        document.querySelector("#nombreContacto").textContent = texts[lang].nombreContacto;
+        document.querySelector("#mensajeContacto").textContent = texts[lang].mensajeContacto;
+        document.querySelector("#botonEnviar").textContent = texts[lang].botonEnviar;
+
+        
         
     });
 });
